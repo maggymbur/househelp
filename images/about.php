00987ba671eff2d031f7955c619f107c6fa793cc
maggymbur <mburumaggy62@gmail.com>
2023-02-18 16:13:23 +0300

    first commit

---
+++ images/about.php
@@ -0,0 +1,21 @@
+<?php
+  $con = mysqli_connect('localhost','root');
+
+  if($con) {
+      echo "connection successful";
+  }
+  else {
+      echo"connection failed";
+  }
+
+  mysqli_select_db($con, 'homly');
+
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $number = $_POST['number'];
+
+  $query = "INSERT INTO users (name, email, number) VALUES('$name', '$email', '$number' )";
+
+  mysqli_query($con, $query);
+  header('location:index.html#contact')
+?>
